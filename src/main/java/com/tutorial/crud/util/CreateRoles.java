4d3fa1b982f7d9a9b6a2b9195896b741b0f3d718
cavanosa <gato.cavanosa@gmail.com>
2020-04-14 13:15:10 +0200

    second commit

---
+++ src/main/java/com/tutorial/crud/util/CreateRoles.java
@@ -0,0 +1,31 @@
+package com.tutorial.crud.util;
+
+import com.tutorial.crud.security.entity.Rol;
+import com.tutorial.crud.security.enums.RolNombre;
+import com.tutorial.crud.security.service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * MUY IMPORTANTE: ESTA CLASE SÓLO SE EJECUTARÁ UNA VEZ PARA CREAR LOS ROLES.
+ * UNA VEZ CREADOS SE DEBERÁ ELIMINAR O BIEN COMENTAR EL CÓDIGO
+ *
+ */
+
+@Component
+public class CreateRoles implements CommandLineRunner {
+
+    @Autowired
+    RolService rolService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        /** Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+        rolService.save(rolAdmin);
+        rolService.save(rolUser);
+         **/
+    }
+}
