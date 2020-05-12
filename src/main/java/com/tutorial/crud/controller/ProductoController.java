2b51d99db0fc3ef77c9531b1712801bfd9aae4be
cavanosa <43098011+cavanosa@users.noreply.github.com>
2020-05-12 14:23:33 +0200

    Update ProductoController.java

--- src/main/java/com/tutorial/crud/controller/ProductoController.java
+++ src/main/java/com/tutorial/crud/controller/ProductoController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/producto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class ProductoController {
 
     @Autowired
